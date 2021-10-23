6e7f4c054c0ab528eb30dc65e3c4ef89a4b86b29
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-10-23 20:08:36 +0100

    Create EconomyCog class
    methods remain unimplemented

---
+++ cogs/economy_cog.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+
+class EconomyCog(commands.Cog):
+    def __init__(self):
+        #get database connection
+        pass
+
+    @commands.group(pass_context=True, invoke_without_command=True)
+    def economy(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("<@521285684271513601> Implement a help message system already you lazy bitch")
+
+    @economy.command()
+    def balance(self, ctx):
+        pass
+
+    @economy.command()
+    def bal(self, ctx):
+        self.balance(ctx)
+
+    @economy.command()
+    def baltop(self, ctx, check_users=10):
+        pass
+
+    @economy.command()
+    def checkbalance(self, ctx, user):
+        pass
+
+    @economy.command()
+    def pay(self, ctx, user):
+        pass
+        
