8769e0eca9154750eb1712172405bdc7180512ec
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-10-25 22:40:24 +0100

    economy cog now tests for and gets database cog as a dependency

--- cogs/cog_economy.py
+++ cogs/cog_economy.py
@@ -1,32 +1,42 @@
 from discord.ext import commands
 from cogs.base_cog import BaseCog
+from cogs.cog_cog_manager import DependencyUnmetError
 
 class cog_economy(BaseCog):
     def __init__(self, client):
-        #get database connection
-        pass
+        db_cog = client.get_cog("cog_database")
+        if db_cog is None:
+            #raise an exception if it's not present
+            raise DependencyUnmetError("Dependency `cog_database` Unmet in `cog_economy!`")
+
+    def setup(self, client):
+        #get database cog
+        self.db_cog = client.get_cog("cog_database")
+        if self.db_cog is None:
+            #raise an exception if it's not present
+            raise DependencyUnmetError("Dependency `cog_database` Unmet in `cog_economy!`")
 
     @commands.group(pass_context=True, invoke_without_command=True)
-    def economy(self, ctx):
+    async def economy(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send("<@521285684271513601> Implement a help message system already you lazy bitch")
 
     @economy.command()
-    def balance(self, ctx):
+    async def balance(self, ctx):
         pass
 
     @economy.command()
-    def bal(self, ctx):
+    async def bal(self, ctx):
         self.balance(ctx)
 
     @economy.command()
-    def baltop(self, ctx, check_users=10):
+    async def baltop(self, ctx, check_users=10):
         pass
 
     @economy.command()
-    def checkbalance(self, ctx, user):
+    async def checkbalance(self, ctx, user):
         pass
 
     @economy.command()
-    def pay(self, ctx, user):
+    async def pay(self, ctx, user):
         pass
